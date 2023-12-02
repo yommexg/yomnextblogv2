e70f73e007e3d8d0cf6757cf6c54c8afbdb127c7
Yomi-Olugbodi Boluwatife <animecodelord@gmail.com>
2023-12-02 02:11:11 +0100

    Remove the local host

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "http://localhost:3000",
+  siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
